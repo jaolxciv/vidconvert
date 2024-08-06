867f05aeb8da9fcf589c561644c5734815ba1f60
Victoria Pelaez <victoria.pelaez@devsense.io>
2024-08-06 16:43:31 +0200

    revision html and js

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,27 @@
-console.log("hola")
+
 document.querySelectorAll('.dropdown-button').forEach(button => {
-    console.log(button)
     button.addEventListener('click', function() {
         const dropdownContent = this.nextElementSibling;
         dropdownContent.classList.toggle('show');
         dropdownContent.style.maxHeight = dropdownContent.classList.contains('show') 
-        console.log(dropdownContent)
         ? dropdownContent.scrollHeight + 'px' 
             : null;
     });
 });
+
+/* ESTE ES EL MISMO CODIGO PERO MAS FACIL DE LEER, LA FUNCION CON EL ? Y LOS : ES DE ORDEN SUPERIOR, ES GUAY PERO PRIMERO VAMOS CON EL JAVASCRIPT BASICO Y MAS FACIL JEJEJE */
+
+/* document.querySelectorAll('.dropdown-button').forEach(button => {
+    button.addEventListener('click', function() {
+        const dropdownContent = this.nextElementSibling;
+        dropdownContent.classList.toggle('show');
+        
+        if (dropdownContent.classList.contains('show')) {
+            dropdownContent.style.maxHeight = dropdownContent.scrollHeight + 'px';
+        } else {
+            dropdownContent.style.maxHeight = null;
+        }
+
+        console.log(dropdownContent);
+    });
+}); */
