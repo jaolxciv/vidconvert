f8db0d1352b4d79758bbdf161c49a683b3ba8762
jaolxciv <jonathanalejandro1508@gmail.com>
2024-08-07 14:51:39 +0200

    change <a> with <buttons> and give function with js - add pointer - add scroll up with js- add summary html/css - add margin to labels up to media query 1216px and 1440px

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,42 @@
-console.log("hola")
-document.querySelectorAll('.dropdown-button').forEach(button => {
-    console.log(button)
-    button.addEventListener('click', function() {
-        const dropdownContent = this.nextElementSibling;
-        dropdownContent.classList.toggle('show');
-        dropdownContent.style.maxHeight = dropdownContent.classList.contains('show') 
-        console.log(dropdownContent)
-        ? dropdownContent.scrollHeight + 'px' 
-            : null;
-    });
+//DOWNLOAD BUTTONS
+var botones = document.querySelectorAll(".download-btn");
+botones.forEach(function (boton) {
+  boton.addEventListener("click", function () {
+    var url = boton.getAttribute("data-url");
+    window.location.href = url;
+  });
 });
+// DOWNLOAD BUTTONS
+
+// SCROLL UP BUTTON
+  const topScrollBtn = document.querySelector(".top-scroll-btn");
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 100) {                      //si se hace un scroll vertical (Y) mayor que 100px, agregar clase show
+      topScrollBtn.classList.add("show");
+    } else {
+      topScrollBtn.classList.remove("show");
+    }
+  });
+
+  topScrollBtn.addEventListener("click", function() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+//SCROLL UP BUTTON
+
+
+//BOTON VIEJO QUE DABA ERROR
+// console.log("hola")
+// document.querySelectorAll(".dropdown-button").forEach(button => {
+//     console.log(button)
+//     button.addEventListener("click", function() {
+//         const dropdownContent = this.nextElementSibling;
+//         dropdownContent.classList.toggle("show");
+//         dropdownContent.style.maxHeight = dropdownContent.classList.contains("show") 
+//         console.log(dropdownContent)
+//         ? dropdownContent.scrollHeight + "px" 
+//             : null;
+//     });
+// });
